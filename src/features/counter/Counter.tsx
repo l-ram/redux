a59cb5d1411cb4ae253a5d3ec71480c559727e05
l-ram <larryramjutton@gmail.com>
2024-08-03 16:38:16 +0200

    wip

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+import { RootState } from "../../store.ts";
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
